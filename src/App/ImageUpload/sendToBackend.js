2ca513dc12e0859e73bc89a2a66a64dab0665ad6
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-02-23 11:31:34 -0800

    upload image to firebase

--- src/App/ImageUpload/sendToBackend.js
+++ src/App/ImageUpload/sendToBackend.js
@@ -3,8 +3,11 @@ import { storage } from '../../Firebase/index'
 const sendToBackend = files => new Promise(async (resolve, reject) => {
 	try {
 		console.log(files)
-		// const imgRef = storage.child('screenshot.jpg')
-		// const result = await imgRef.put()
+		const [file] = files
+		console.log(file)
+		const imgRef = storage.child('screenshot.png')
+		const result = await imgRef.put(file)
+		console.log(result)
 		resolve('ok')
 	} catch (error) {
 		if (error.response) console.log(error.response)
