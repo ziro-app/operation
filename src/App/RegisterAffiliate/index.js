083f31929ce243ab1f409fca2d40636c9291d6ca
Wermeson Rocha <wermesonrocha2@gmail.com>
2020-03-08 01:35:21 -0300

    First implementation adding affiliates

---
+++ src/App/RegisterAffiliate/index.js
@@ -0,0 +1,139 @@
+import React, { useState, useEffect } from 'react'
+import fetch from './fetch'
+import sendToBackend from './sendToBackend'
+import maskInput from '@ziro/mask-input'
+import capitalize from '@ziro/capitalize'
+import InitialLoader from '@bit/vitorbarbosa19.ziro.initial-loader'
+import Error from '@bit/vitorbarbosa19.ziro.error'
+import HeaderHome from '@bit/vitorbarbosa19.ziro.header-home'
+import Form from '@bit/vitorbarbosa19.ziro.form'
+import FormInput from '@bit/vitorbarbosa19.ziro.form-input'
+import Dropdown from '@bit/vitorbarbosa19.ziro.dropdown'
+import InputText from '@bit/vitorbarbosa19.ziro.input-text'
+import { containerWithPadding } from '@ziro/theme'
+import { welcome, marker } from './styles'
+
+const RegisterAffiliate = () => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [isError, setIsError] = useState(false)
+    // dropdown options
+    const [brandsAndBranches, setBrandsAndBranches] = useState([])
+    const [brands, setBrands] = useState([])
+    const [branches, setBranches] = useState([])
+    // form fields
+    const [brand, setBrand] = useState('')
+    const [branch, setBranch] = useState('')
+    const [insta, setInsta] = useState('')
+    const [fname, setFname] = useState('')
+    const [lname, setLname] = useState('')
+    const [cpf, setCpf] = useState('')
+    const [whats, setWhats] = useState('')
+    const state = { brand, branch, insta, fname, lname, cpf, whats }
+    useEffect(() => fetch(setIsLoading, setIsError, setBrands, setBrandsAndBranches), [])
+    useEffect(() => {
+        setBranch('')
+        setBranches(brandsAndBranches
+            .filter(value => value.split(' - ')[0] === brand)
+            .map(value => value.split(' - ')[1].replace('Bom Retiro', 'B. Retiro')))
+    }, [brand])
+    const validations = [
+        {
+            name: 'brand',
+            validation: value => !!value,
+            value: brand,
+            message: 'Marca inválida'
+        }, {
+            name: 'branch',
+            validation: value => !!value,
+            value: branch,
+            message: 'Unidade inválida'
+        }, {
+            name: 'fname',
+            validation: value => !!value,
+            value: fname,
+            message: 'Campo obrigatório'
+        }, {
+            name: 'lname',
+            validation: value => !!value,
+            value: lname,
+            message: 'Campo obrigatório'
+        }, {
+            name: 'cpf',
+            validation: value => value.length >= 14,
+            value: cpf,
+            message: 'Formato inválido'
+        }, {
+            name: 'whats',
+            validation: value => value.length >= 14,
+            value: whats,
+            message: 'Formato inválido'
+        }
+    ]
+    if (isLoading) return <InitialLoader />
+    if (isError) return <Error />
+    return (
+        <>
+            <Form
+                validations={validations}
+                sendToBackend={sendToBackend ? sendToBackend(state) : () => null}
+                inputs={[
+                    <FormInput name='brand' label='Marca' input={
+                        <Dropdown
+                            value={brand}
+                            onChange={({ target: { value } }) => setBrand(value)}
+                            list={brands}
+                            placeholder='Marca onde trabalha'
+                            onChangeKeyboard={element => element ? setBrand(element.value) : null}
+                        />
+                    } />,
+                    <FormInput name='branch' label='Unidade da Marca' input={
+                        <Dropdown
+                            value={branch}
+                            onChange={({ target: { value } }) => setBranch(value)}
+                            list={branches || ['Escolha uma marca acima']}
+                            placeholder='Unidade/endereço da marca'
+                            onChangeKeyboard={element => element ? setBranch(element.value) : null}
+                        />
+                    } />,
+                    <FormInput name='insta' label='Instagram da Marca' input={
+                        <InputText
+                            value={insta}
+                            onChange={({ target: { value } }) => setInsta(value)}
+                            placeholder='Ex.: ateliederoupa. Não use .com'
+                        />
+                    } />,
+                    <FormInput name='fname' label='Nome' input={
+                        <InputText
+                            value={fname}
+                            onChange={({ target: { value } }) => setFname(capitalize(value))}
+                            placeholder='Seu primeiro nome'
+                        />
+                    } />,
+                    <FormInput name='lname' label='Sobrenome' input={
+                        <InputText
+                            value={lname}
+                            onChange={({ target: { value } }) => setLname(capitalize(value))}
+                            placeholder='Seu sobrenome'
+                        />
+                    } />,
+                    <FormInput name='cpf' label='CPF' input={
+                        <InputText
+                            value={cpf}
+                            onChange={({ target: { value } }) => setCpf(maskInput(value, '###.###.###-##', true))}
+                            placeholder='000.111.222-33'
+                        />
+                    } />,
+                    <FormInput name='whats' label='Whatsapp' input={
+                        <InputText
+                            value={whats}
+                            onChange={({ target: { value } }) => setWhats(maskInput(value, '(##) #####-####', true))}
+                            placeholder='(11) 91122-3344'
+                        />
+                    } />
+                ]}
+            />
+        </>
+    )
+}
+
+export default RegisterAffiliate
--- webpack.config.js
+++ webpack.config.js
@@ -5,97 +5,100 @@ const WebpackPwaManifest = require('webpack-pwa-manifest')
 const webpack = require('webpack')
 
 module.exports = (env, { mode }) => {
-	const config = {
-		module: {
-			rules: [
-				{
-					test: /\.js$/,
-					exclude: /node_modules/,
-					use: {
-						loader: 'babel-loader',
-						options: {
-							presets: ['@babel/preset-env', '@babel/preset-react'],
-							plugins: ['@babel/plugin-transform-runtime']
-						}
-					}
-				},
-				{
-					test: /\.css$/,
-					use: ['style-loader', 'raw-loader']
-				}
-			]
-		},
-		plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })]
-	}
-	if (mode === 'development') {
-		const {
-			sheet_url,
-			sheet_token,
-			sheet_id,
-			continue_url,
-			sheet_storeowners_id,
-			sheet_id_refer_legacy,
-			sheet_id_affiliates,
-			sheet_id_people,
-			sheet_id_brands,
-			cnpj_url,
-			cnpj_token
-		} = require('./credentials')
-		config.devtool = 'cheap-module-eval-source-map'
-		config.devServer = { historyApiFallback: true }
-		config.plugins.push(
-			new webpack.DefinePlugin({
-				'process.env': {
-					SHEET_URL: JSON.stringify(sheet_url),
-					SHEET_TOKEN: JSON.stringify(sheet_token),
-					SHEET_ID: JSON.stringify(sheet_id),
-					CONTINUE_URL: JSON.stringify(continue_url),
-					SHEET_STOREOWNERS_ID: JSON.stringify(sheet_storeowners_id),
-					SHEET_ID_REFER_LEGACY: JSON.stringify(sheet_id_refer_legacy),
-					SHEET_ID_AFFILIATES: JSON.stringify(sheet_id_affiliates),
-					SHEET_ID_PEOPLE: JSON.stringify(sheet_id_people),
-					SHEET_ID_BRANDS: JSON.stringify(sheet_id_brands),
-					CNPJ_URL: JSON.stringify(cnpj_url),
-					CNPJ_TOKEN: JSON.stringify(cnpj_token)
-				}
-			})
-		)
-	}
-	if (mode === 'production') {
-		config.devtool = 'cheap-module-source-map'
-		config.plugins.push(
-			new CompressionPlugin(),
-			new CopyWebpackPlugin([
-				{ from: './_redirects', to: '_redirects', toType: 'file' },
-				{ from: './src/sw.js', to: 'sw.js', toType: 'file' }
-			]),
-			new WebpackPwaManifest({
-				name: 'Interno',
-				short_name: 'Interno',
-				start_url: '/',
-				background_color: '#FFF',
-				theme_color: '#FFF',
-				display: 'standalone',
-				icons: [{ src: './logo.png', sizes: [96, 128, 192, 256, 384, 512] }]
-			}),
-			new webpack.DefinePlugin({
-				'process.env': {
-					SHEET_URL: JSON.stringify(process.env.SHEET_URL),
-					SHEET_TOKEN: JSON.stringify(process.env.SHEET_TOKEN),
-					SHEET_ID: JSON.stringify(process.env.SHEET_ID),
-					CONTINUE_URL: JSON.stringify(process.env.CONTINUE_URL),
-					SHEET_STOREOWNERS_ID: JSON.stringify(process.env.SHEET_STOREOWNERS_ID),
-					SHEET_ID_REFER_LEGACY: JSON.stringify(process.env.SHEET_ID_REFER_LEGACY),
-					SHEET_ID_AFFILIATES: JSON.stringify(process.env.SHEET_ID_AFFILIATES),
-					SHEET_ID_PEOPLE: JSON.stringify(process.env.SHEET_ID_PEOPLE),
-					SHEET_ID_BRANDS: JSON.stringify(process.env.SHEET_ID_BRANDS),
-					CNPJ_URL: JSON.stringify(process.env.CNPJ_URL),
-					CNPJ_TOKEN: JSON.stringify(process.env.CNPJ_TOKEN)
-				}
-			})
-		)
-	}
-	return config
+    const config = {
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react'],
+                            plugins: ['@babel/plugin-transform-runtime']
+                        }
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'raw-loader']
+                }
+            ]
+        },
+        plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })]
+    }
+    if (mode === 'development') {
+        const {
+            sheet_url,
+            sheet_token,
+            sheet_id,
+            continue_url,
+            sheet_storeowners_id,
+            sheet_id_refer_legacy,
+            sheet_id_affiliates,
+            sheet_id_people,
+            sheet_id_brands,
+            sheet_id_register_get,
+            cnpj_url,
+            cnpj_token
+        } = require('./credentials')
+        config.devtool = 'cheap-module-eval-source-map'
+        config.devServer = { historyApiFallback: true }
+        config.plugins.push(
+            new webpack.DefinePlugin({
+                'process.env': {
+                    SHEET_URL: JSON.stringify(sheet_url),
+                    SHEET_TOKEN: JSON.stringify(sheet_token),
+                    SHEET_ID: JSON.stringify(sheet_id),
+                    CONTINUE_URL: JSON.stringify(continue_url),
+                    SHEET_STOREOWNERS_ID: JSON.stringify(sheet_storeowners_id),
+                    SHEET_ID_REFER_LEGACY: JSON.stringify(sheet_id_refer_legacy),
+                    SHEET_ID_AFFILIATES: JSON.stringify(sheet_id_affiliates),
+                    SHEET_ID_PEOPLE: JSON.stringify(sheet_id_people),
+                    SHEET_ID_BRANDS: JSON.stringify(sheet_id_brands),
+                    SHEET_ID_REGISTER_GET: JSON.stringify(sheet_id_register_get),
+                    CNPJ_URL: JSON.stringify(cnpj_url),
+                    CNPJ_TOKEN: JSON.stringify(cnpj_token)
+                }
+            })
+        )
+    }
+    if (mode === 'production') {
+        config.devtool = 'cheap-module-source-map'
+        config.plugins.push(
+            new CompressionPlugin(),
+            new CopyWebpackPlugin([
+                { from: './_redirects', to: '_redirects', toType: 'file' },
+                { from: './src/sw.js', to: 'sw.js', toType: 'file' }
+            ]),
+            new WebpackPwaManifest({
+                name: 'Interno',
+                short_name: 'Interno',
+                start_url: '/',
+                background_color: '#FFF',
+                theme_color: '#FFF',
+                display: 'standalone',
+                icons: [{ src: './logo.png', sizes: [96, 128, 192, 256, 384, 512] }]
+            }),
+            new webpack.DefinePlugin({
+                'process.env': {
+                    SHEET_URL: JSON.stringify(process.env.SHEET_URL),
+                    SHEET_TOKEN: JSON.stringify(process.env.SHEET_TOKEN),
+                    SHEET_ID: JSON.stringify(process.env.SHEET_ID),
+                    CONTINUE_URL: JSON.stringify(process.env.CONTINUE_URL),
+                    SHEET_STOREOWNERS_ID: JSON.stringify(process.env.SHEET_STOREOWNERS_ID),
+                    SHEET_ID_REFER_LEGACY: JSON.stringify(process.env.SHEET_ID_REFER_LEGACY),
+                    SHEET_ID_AFFILIATES: JSON.stringify(process.env.SHEET_ID_AFFILIATES),
+                    SHEET_ID_PEOPLE: JSON.stringify(process.env.SHEET_ID_PEOPLE),
+                    SHEET_ID_BRANDS: JSON.stringify(process.env.SHEET_ID_BRANDS),
+                    SHEET_ID_REGISTER_GET: JSON.stringify(process.env.SHEET_ID_REGISTER_GET),
+                    CNPJ_URL: JSON.stringify(process.env.CNPJ_URL),
+                    CNPJ_TOKEN: JSON.stringify(process.env.CNPJ_TOKEN)
+                }
+            })
+        )
+    }
+    return config
 }
 
 // tentar dar os bit remove e tals, dps o bit import
