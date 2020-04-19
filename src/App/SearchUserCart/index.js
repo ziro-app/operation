727cd3a977a2ac7faa6754b0dc1c5366560eb76d
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-04-19 08:56:04 -0700

    start updating SearchUserCart

--- src/App/SearchUserCart/index.js
+++ src/App/SearchUserCart/index.js
@@ -1,55 +1,65 @@
 import React, { useState, useCallback } from 'react'
 import { useLocation } from 'wouter'
 import Spinner from '@bit/vitorbarbosa19.ziro.spinner-with-div'
-import Input from '@bit/vitorbarbosa19.ziro.input-text'
+import Form from '@bit/vitorbarbosa19.ziro.form'
+import FormInput from '@bit/vitorbarbosa19.ziro.form-input'
+import Dropdown from '@bit/vitorbarbosa19.ziro.dropdown'
 import Button from '@bit/vitorbarbosa19.ziro.button'
 import { db } from '../../Firebase'
 import { fontTitle } from '@ziro/theme'
 
 export default () => {
-    const [l,setLocation] = useLocation()
-    const [email, setEmail] = useState()
+    const [,setLocation] = useLocation()
+    const [razao, setRazao] = useState('')
     const [notFound, setNotFound] = useState(false)
     const [searching, setSearching] = useState(false)
 
-    const search = useCallback(() => {
-        setNotFound(false)
-        setSearching(true)
-        db.collection('storeowners')
-            .where('email','==',email)
-            .get()
-            .then(snap => {
-                if(!snap.empty) {
-                    setLocation(`/pedidos/${snap.docs[0].id}`)
-                }
-                else {
-                    setNotFound(true)
-                    setSearching(false)
-                }
-            })
-    },[email])
-
+    // const search = useCallback(() => {
+    //     setNotFound(false)
+    //     setSearching(true)
+    //     db.collection('storeowners')
+    //         .where('email','==',email)
+    //         .get()
+    //         .then(snap => {
+    //             if(!snap.empty) {
+    //                 setLocation(`/pedidos/${snap.docs[0].id}`)
+    //             }
+    //             else {
+    //                 setNotFound(true)
+    //                 setSearching(false)
+    //             }
+    //         })
+    // },[email])
+    const validations = [
+        {
+            name: 'razao',
+            validation: value => ['razão 1','razão 2'].includes(razao),
+            value: razao,
+            message: 'Razão inválida'
+        }
+    ]
     return (
         <div style={{ display: 'grid', gridGap: '20px' }}>
-            <label>Digite o email do lojista para ver seus pedidos</label>
-            <Input
-                placeholder='lojista@exemplo.com'
-                value={email}
-                onChange={({ target: { value } }) => setEmail(value)}
+            <label>Procure o pedido pela razão social do lojista</label>
+            <Form
+                buttonOnTop={true}
+                buttonName='Procurar pedido'
+                validations={validations}
+                sendToBackend={() => null}
+                // sendToBackend={sendToBackend ? sendToBackend(state) : () => null}
+                inputs={[
+                    <FormInput name='razao' label='Razão social' input={
+                        <Dropdown
+                            value={razao}
+                            submitting={searching}
+                            onChange={({ target: { value } }) => setRazao(value)}
+                            onChangeKeyboard={element => element ? setRazao(element.value) : null}
+                            list={['razão 1','razão 2']}
+                            placeholder='Razão do lojista'
+                        />
+                    } />,
+                ]}
             />
-            <Button
-                type='button'
-                cta='Procurar'
-                click={search}
-            />
-            { searching && <Spinner /> }
-            { !searching && notFound && 
-                <label style={{
-                    marginTop: '20px',
-                    fontFamily: fontTitle,
-                    fontSize: '1.5rem',
-                    textAlign: 'center'
-                }}>Nenhum lojista encontrado</label> }
         </div>
     )
 }
