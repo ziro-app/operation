42643cb37dd622abab5c6e032e8cff1a25f9ba5e
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-04-26 16:48:20 -0700

    finish fetching storeowners with open orders

--- src/App/SearchUserCart/fetch.js
+++ src/App/SearchUserCart/fetch.js
@@ -7,7 +7,7 @@ const removeDuplicates = arrayWithDuplicates => {
 	}, [])
 }
 
-const fetch = setIsLoading => {
+const fetch = (setIsLoading, setIsError, setStoreowners) => {
 		// return db.collection('catalog-user-data').where('cart','==',true).onSnapshot(snap =>
 		// 	snap.forEach(async users => {
 		// 		const items = await db.collection('catalog-user-data').doc(users.id).collection('cart').get()
@@ -25,9 +25,11 @@ const fetch = setIsLoading => {
 				const customer = await db.collection('storeowners').doc(ref).get()
 				return customer.data()
 			}))
-			console.log(customers)
+			const storeowners = customers && customers.map(customer => customer?.razao).sort((a,b) => a < b ? -1 : 1)
+			setStoreowners(storeowners)
 		} catch (error) {
 			console.log(error)
+			setIsError(true)
 		} finally {
 			setIsLoading(false)		
 		}
@@ -36,41 +38,3 @@ const fetch = setIsLoading => {
 }
 
 export default fetch
-
-// import axios from 'axios'
-
-// const fetch = (setIsLoading, setIsError, setStoreowners) => {
-// 	const source = axios.CancelToken.source()
-// 	const run = async () => {
-// 		const config = {
-// 			method: 'POST',
-// 			url: process.env.SHEET_URL,
-// 			data: {
-// 				apiResource: 'values',
-// 				apiMethod: 'get',
-// 				spreadsheetId: process.env.SHEET_STOREOWNERS_ID,
-// 				range: 'Base!K2:K'
-// 			},
-// 			headers: {
-// 				'Authorization': process.env.SHEET_TOKEN,
-// 				'Content-Type': 'application/json'
-// 			},
-// 			cancelToken: source.token
-// 		}
-// 		try {
-// 			const { data: { values } } = await axios(config)
-// 			const storeowners = values.flat().sort((a,b) => a < b ? -1 : 1)
-// 			setStoreowners(storeowners)
-// 		} catch (error) {
-// 			if (error.response) console.log(error.response)
-// 			else console.log(error)
-// 			setIsError(true)
-// 		} finally {
-// 			setIsLoading(false)
-// 		}
-// 	}
-// 	run()
-// 	return () => source.cancel('Canceled fetch request. Component unmounted')
-// }
-
-// export default fetch
