fd9ac54953c88721aad5b7ce3ea8914555026404
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-04-26 19:38:23 -0700

    refactoring

--- src/App/SearchUserCart/fetch.js
+++ src/App/SearchUserCart/fetch.js
@@ -1,40 +1,30 @@
 import { db } from '../../Firebase'
-
-const removeDuplicates = arrayWithDuplicates => {
-	return arrayWithDuplicates.reduce((accumulated, current) => {
-		if (accumulated.includes(current)) return accumulated
-		return [...accumulated, current]
-	}, [])
-}
+import removeDuplicates from '@ziro/remove-duplicates'
 
 const fetch = (setIsLoading, setIsError, setStoreowners) => {
-		// return db.collection('catalog-user-data').where('cart','==',true).onSnapshot(snap =>
-		// 	snap.forEach(async users => {
-		// 		const items = await db.collection('catalog-user-data').doc(users.id).collection('cart').get()
-		// 		items.forEach(doc => console.log(doc.data()))
-		// 	})
-		// )
-		// return db.collectionGroup('cart').where('status','==','open')
-	const run = async () => {
-		try {
-			const orders = await db.collectionGroup('cart').where('status','==','open').get()
-			const customerRefs = []
-			orders.forEach(doc => customerRefs.push(doc.ref.parent.parent.id))
-			const dedupedCustomerRefs = removeDuplicates(customerRefs)
-			const customers = await Promise.all(dedupedCustomerRefs.map(async ref => {
-				const customer = await db.collection('storeowners').doc(ref).get()
-				return customer.data()
-			}))
-			const storeowners = customers && customers.map(customer => customer?.razao).sort((a,b) => a < b ? -1 : 1)
-			setStoreowners(storeowners)
-		} catch (error) {
-			console.log(error)
-			setIsError(true)
-		} finally {
-			setIsLoading(false)		
-		}
+	try {
+		return db.collectionGroup('cart').where('status','==','open').onSnapshot(async orders => {
+			try {
+				const customerRefs = []
+				orders.forEach(doc => customerRefs.push(doc.ref.parent.parent.id))
+				const dedupedCustomerRefs = removeDuplicates(customerRefs)
+				const customers = await Promise.all(dedupedCustomerRefs.map(async ref => {
+					const customer = await db.collection('storeowners').doc(ref).get()
+					return customer.data()
+				}))
+				const storeowners = customers && customers.map(customer => customer?.razao).sort((a,b) => a < b ? -1 : 1)
+				setStoreowners(storeowners)
+			} catch (error) {
+				console.log(error)
+				setIsError(true)
+			}
+		})
+	} catch (error) {
+		console.log(error)
+		setIsError(true)
+	} finally {
+		setIsLoading(false)
 	}
-	run()
 }
 
 export default fetch
