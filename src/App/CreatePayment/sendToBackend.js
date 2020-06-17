ce912efe33f23b5bd917c7b141d74571cc561e8c
Wermeson Rocha <wermesonrocha2@gmail.com>
2020-06-17 11:20:50 -0300

    Adding permission

--- src/App/CreatePayment/sendToBackend.js
+++ src/App/CreatePayment/sendToBackend.js
@@ -1,50 +1,50 @@
 import { db } from '../../Firebase/index';
 
 const sendToBackend = state => () => {
-  const { nickname, seller, sellerId, charge, maxInstallments, setFantasy, setCharge, setMaxInstallments } = state;
-  const nome = nickname ? nickname.trim() : '';
-  const baseUrl = 'https://ziro.app/pagamento/';
-  return new Promise(async (resolve, reject) => {
-    try {
-      if (nome === 'Uiller' || nome === 'Vitor') {
-        if (seller && sellerId) {
-          const docRef = await db.collection('credit-card-payments').add({
-            dateLinkCreated: new Date(),
-            seller,
-            sellerZoopId: sellerId,
-            charge,
-            maxInstallments,
-            status: 'Aguardando Pagamento',
-          });
-          try {
-            const doc = await docRef.get();
-            if (doc) await navigator.clipboard.writeText(`${baseUrl}${doc.id}/escolher-cartao?doc`);
-          } catch (error) {
-            throw { msg: 'Erro ao realizar a cópia', copyError: true };
-          }
-          resolve('Link copiado');
-          setFantasy('');
-          setCharge('');
-          setMaxInstallments('');
-        } else {
-          throw { msg: 'Vendedor não encontrado', customError: true };
+    const { nickname, seller, sellerId, charge, maxInstallments, setFantasy, setCharge, setMaxInstallments } = state;
+    const nome = nickname ? nickname.trim() : '';
+    const baseUrl = 'https://ziro.app/pagamento/';
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (nome === 'Uiller' || nome === 'Vitor' || nome === 'Bruno') {
+                if (seller && sellerId) {
+                    const docRef = await db.collection('credit-card-payments').add({
+                        dateLinkCreated: new Date(),
+                        seller,
+                        sellerZoopId: sellerId,
+                        charge,
+                        maxInstallments,
+                        status: 'Aguardando Pagamento',
+                    });
+                    try {
+                        const doc = await docRef.get();
+                        if (doc) await navigator.clipboard.writeText(`${baseUrl}${doc.id}/escolher-cartao?doc`);
+                    } catch (error) {
+                        throw { msg: 'Erro ao realizar a cópia', copyError: true };
+                    }
+                    resolve('Link copiado');
+                    setFantasy('');
+                    setCharge('');
+                    setMaxInstallments('');
+                } else {
+                    throw { msg: 'Vendedor não encontrado', customError: true };
+                }
+            } else throw { msg: 'Permissão insuficiente', customError: true };
+        } catch (error) {
+            if (error.copyError) {
+                resolve('Link criado. Acesse na aba de vendas');
+                setFantasy('')
+                setCharge('');
+                setMaxInstallments('');
+            }
+            if (error.customError) reject(error);
+            else {
+                console.log(error);
+                if (error.response) console.log(error.response);
+                reject('Erro ao criar cobrança');
+            }
         }
-      } else throw { msg: 'Permissão insuficiente', customError: true };
-    } catch (error) {
-      if (error.copyError) {
-        resolve('Link criado. Acesse na aba de vendas');
-        setFantasy('')
-        setCharge('');
-        setMaxInstallments('');
-      }
-      if (error.customError) reject(error);
-      else {
-        console.log(error);
-        if (error.response) console.log(error.response);
-        reject('Erro ao criar cobrança');
-      }
-    }
-  });
+    });
 };
 
 export default sendToBackend;
