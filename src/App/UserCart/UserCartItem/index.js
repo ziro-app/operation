ede506d5c8e0b5b77538b5a5fb5a92dac2b4fef1
Alessandro <vyseclown@gmail.com>
2020-07-08 13:07:44 -0300

    Minor fix
    Signed-off-by: Alessandro <vyseclown@gmail.com>

--- src/App/UserCart/UserCartItem/index.js
+++ src/App/UserCart/UserCartItem/index.js
@@ -6,45 +6,40 @@ import JSZip from 'jszip';
 import Header from '@bit/vitorbarbosa19.ziro.header';
 import Button from '@bit/vitorbarbosa19.ziro.button';
 import { brandCart, brandName, buttonDownload } from './styles';
-import CardForm from '@bit/vitorbarbosa19.ziro.card-form';
+//import CardForm from '@bit/vitorbarbosa19.ziro.card-form';
+import Card from './card';
 import currencyFormat from '@ziro/currency-format';
 import { priceTotal, saleSummary, summary, total } from './styles_catalog';
 import { containerWithPadding } from '@ziro/theme';
 import { reduceTotal } from './utils';
 
-export default ({
-                    cart: { productIds, products, ...cart },
-                    storeowner,
-                    oldQuery,
-                }) => {
+export default ({ cart: { productIds, products, ...cart }, storeowner, oldQuery }) => {
     const [prices, setPrices] = useState({});
     const [urls, setURLs] = useState({});
     const [location, setLocation] = useLocation();
     const [match, params] = useRoute('/pedidos/:cartId?');
 
-    const [totalItems, totalPrice] = useMemo(
-        () =>
-            productIds && products
-                ? productIds.reduce(reduceTotal(prices, products), [0, 0])
-                : [0, 0],
-        [productIds, products, prices]
-    );
+    const [totalItems, totalPrice] = useMemo(() => (productIds && products ? productIds.reduce(reduceTotal(prices, products), [0, 0]) : [0, 0]), [
+        productIds,
+        products,
+        prices,
+    ]);
 
     const confirmCartItem = useCallback(async () => {
         try {
             await db
-                .collection("catalog-user-data")
+                .collection('catalog-user-data')
                 .doc(cart.storeownerId)
-                .collection("cart")
+                .collection('cart')
                 .doc(cart.id)
                 .set(
                     {
-                        status: "waitingPayment",
+                        status: 'waitingPayment',
                         lastUpdate: fs.FieldValue.serverTimestamp(),
-                        updatedBy: "seller",
+                        updatedBy: 'seller',
                         total: `${totalPrice}`,
                     },
-                    { merge: true }
+                    { merge: true },
                 );
         } catch (error) {
             console.log({ error });
@@ -58,24 +53,18 @@ export default ({
             const folder = zip.folder(`${storeowner.razao}`);
             await Promise.all(
                 Object.entries(urls).map(([productId, url]) => {
-                    return get(url, { responseType: "arraybuffer" }).then(
-                        ({ data }) => {
-                            const filename = `peca_${
-                                productIds.indexOf(productId) - 1
-                            }.png`;
-                            const file = new Blob([
-                                Buffer.from(data, "binary"),
-                            ]);
-                            folder.file(filename, file);
-                        }
-                    );
-                })
+                    return get(url, { responseType: 'arraybuffer' }).then(({ data }) => {
+                        const filename = `peca_${productIds.indexOf(productId) - 1}.png`;
+                        const file = new Blob([Buffer.from(data, 'binary')]);
+                        folder.file(filename, file);
+                    });
+                }),
             );
-            const content = await zip.generateAsync({ type: "blob" });
+            const content = await zip.generateAsync({ type: 'blob' });
             const url = window.URL.createObjectURL(content);
-            const link = document.createElement("a");
+            const link = document.createElement('a');
             link.href = url;
-            link.setAttribute("download", `${storeowner.razao}.zip`);
+            link.setAttribute('download', `${storeowner.razao}.zip`);
             document.body.appendChild(link);
             link.click();
         } catch (error) {
@@ -85,72 +74,44 @@ export default ({
 
     return (
         <div style={containerWithPadding}>
-            <Header
-                type="icon-link"
-                title={storeowner.razao}
-                navigateTo={`/pedidos${oldQuery || ""}`}
-                icon="back"
-            />
+            <Header type="icon-link" title={storeowner.razao} navigateTo={`/pedidos${oldQuery || ''}`} icon="back"/>
             <div style={brandCart}>
                 <label style={brandName}>{cart.brandName}</label>
-                <Button
-                    type="button"
-                    cta="Fazer download fotos"
-                    click={downloadAllImages}
-                    style={buttonDownload}
-                />
-                {productIds.map((productId) => (
-                    <CardForm
+                <Button type="button" cta="Fazer download fotos" click={downloadAllImages} style={buttonDownload}/>
+                {productIds.map(productId => (
+                    /*<CardForm
+                                  key={productId}
+                                  productId={productId}
+                                  cartProduct={products[productId]}
+                                  setPrice={(price) =>
+                                      setPrices((old) => ({ ...old, [productId]: price }))
+                                  }
+                                  setURL={(url) =>
+                                      setURLs((old) => ({ ...old, [productId]: url }))
+                                  }
+                              />*/
+                    <Card
                         key={productId}
                         productId={productId}
                         cartProduct={products[productId]}
-                        setPrice={(price) =>
-                            setPrices((old) => ({ ...old, [productId]: price }))
-                        }
-                        setURL={(url) =>
-                            setURLs((old) => ({ ...old, [productId]: url }))
-                        }
+                        setPrice={price => setPrices(old => ({ ...old, [productId]: price }))}
+                        setURL={url => setURLs(old => ({ ...old, [productId]: url }))}
                     />
-                    /*<Card
-                        key={productId}
-                        productId={productId}
-                        cartProduct={products[productId]}
-                        setPrice={(price) =>
-                            setPrices((old) => ({ ...old, [productId]: price }))
-                        }
-                        setURL={(url) =>
-                            setURLs((old) => ({ ...old, [productId]: url }))
-                        }
-                    />*/
                 ))}
                 <div style={summary}>
                     <div style={saleSummary}>
                         <label style={total}>Total da compra</label>
-                        <label style={priceTotal}>
-                            {currencyFormat(totalPrice)}
-                        </label>
+                        <label style={priceTotal}>{currencyFormat(totalPrice)}</label>
                     </div>
                     <div style={saleSummary}>
                         <label style={total}>Quantidade</label>
                         <label style={priceTotal}>{totalItems}</label>
                     </div>
                 </div>
-                {cart.status === "waitingConfirmation" && (
-                    <Button
-                        type="button"
-                        cta={"Confirmar pedido"}
-                        click={confirmCartItem}
-                        submitting={false}
-                    />
-                )}
-                {cart.status === "waitingPayment" && (
-                    <Button
-                        type="button"
-                        cta={"Aguardando pagamento"}
-                        click={() => {}}
-                        submitting={true}
-                    />
-                )}
+                {cart.status === 'waitingConfirmation' &&
+                <Button type="button" cta={'Confirmar pedido'} click={confirmCartItem} submitting={false}/>}
+                {cart.status === 'waitingPayment' && <Button type="button" cta={'Aguardando pagamento'} click={() => {
+                }} submitting={true}/>}
             </div>
         </div>
     );
