dd025a5331d4c98a9d769295d112be97d638c285
Alessandro <vyseclown@gmail.com>
2020-08-07 11:16:54 -0300

    Fixing the test environment
    Signed-off-by: Alessandro <vyseclown@gmail.com>

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -1,17 +1,17 @@
-import * as firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
-import 'firebase/storage'
-import { firebaseConfig } from './firebase-config.js'
-import { firebaseHomologConfig } from './firebase-homolog-config'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+import { firebaseConfig } from './firebase-config.js';
+import { firebaseHomologConfig } from './firebase-homolog-config';
 
-const config = process.env.HOMOLOG ? firebaseHomologConfig : firebaseConfig
+const config = process.env.HOMOLOG ? firebaseHomologConfig : firebaseConfig;
 
-const init = firebase.initializeApp(config)
+const init = firebase.initializeApp(config);
 
-export const
-    db = init.firestore(),
+export const db = init.firestore(),
     auth = init.auth(),
     fs = firebase.firestore,
     fbauth = firebase.auth,
-    storage = firebase.storage().ref()
+    storage = firebase.storage().ref(),
+    configFirebase = config;
