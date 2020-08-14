9cecb0edf87e267de3afd0357bc5d8424fe055a0
Alessandro <vyseclown@gmail.com>
2020-08-14 13:53:43 -0300

    fix: UploadImages now work with more images
    Signed-off-by: Alessandro <vyseclown@gmail.com>

--- src/App/CardForm/index.js
+++ src/App/CardForm/index.js
@@ -1,112 +1,117 @@
-import React, { useState } from 'react';
-import RImg from 'react-image';
-import SpinnerWithDiv from '@bit/vitorbarbosa19.ziro.spinner-with-div';
-import CardInputs from './cardInputs';
+import React, { useState, memo } from 'react'
+import RImg from 'react-image'
+import SpinnerWithDiv from '@bit/vitorbarbosa19.ziro.spinner-with-div'
+import CardInputs from './cardInputs'
 import {
-    fileContainerUploadPictureContainerClass,
-    fileContainerUploadPictureContainerimgUploadPictureClass,
-    fileContainerUploadPicturesWrapperClass,
-    image,
-} from './styles';
-import InfoCard from './infoCard';
-import SummaryCard from './summaryCard';
-import CardControls from './CardControls';
+  fileContainerUploadPictureContainerClass,
+  fileContainerUploadPictureContainerimgUploadPictureClass,
+  fileContainerUploadPicturesWrapperClass,
+  image,
+} from './styles'
+import InfoCard from './infoCard'
+import SummaryCard from './summaryCard'
+import CardControls from './CardControls'
 
 const PTstatus = {
-    available: 'Disponível',
-    unavailable: 'Indisponível',
-    closed: 'Disponível',
-    waitingInfo: '',
-    soldOut: 'Indisponível',
+  available: 'Disponível',
+  unavailable: 'Indisponível',
+  closed: 'Disponível',
+  waitingInfo: '',
+  soldOut: 'Indisponível',
 }
 
 const INstatus = {
-    Disponível: 'available',
-    Indisponível: 'soldOut',
+  Disponível: 'available',
+  Indisponível: 'soldOut',
 }
 
-export default ({
-                    product,
-                    filesList,
-                    setFiles,
-                    index,
-                    picture,
-                    removeImage,
-                    update,
-                    cardInfo = false,
-                    editing,
-                    cartProduct,
-                    setEditing,
-                    validations,
-                    state,
-                    arrayOfInputs,
-                    pictures,
-                    setPictures,
-                    initialStatus,
-                    dispatch,
-                    duplicateImage,
-                    identifierOfPicture,
-                    uuid,
-                    thumbPhoto,
-                    setThumbPhoto,
-                    secondArrayOfInputs,
-                }) => {
-    const [removeImageModal, setRemoveImageModal] = useState(false);
-    const [duplicateImageModal, setDuplicateImageModal] = useState(false);
+export default memo(
+  ({
+    product,
+    filesList,
+    setFiles,
+    index,
+    picture,
+    removeImage,
+    update,
+    cardInfo = false,
+    editing,
+    cartProduct,
+    setEditing,
+    validations,
+    state,
+    arrayOfInputs,
+    pictures,
+    setPictures,
+    initialStatus,
+    dispatch,
+    duplicateImage,
+    identifierOfPicture,
+    uuid,
+    thumbPhoto,
+    setThumbPhoto,
+    secondArrayOfInputs,
+  }) => {
+    console.log('index inside cardForm', index)
+    const [removeImageModal, setRemoveImageModal] = useState(false)
+    const [duplicateImageModal, setDuplicateImageModal] = useState(false)
     return (
-        <div style={fileContainerUploadPicturesWrapperClass} className="uploadPicturesWrapper">
-            <div key={index} style={fileContainerUploadPictureContainerClass} className="uploadPictureContainer">
-                <CardControls
-                    duplicateImage={duplicateImage}
-                    filesList={filesList}
-                    pictures={pictures}
-                    picture={picture}
-                    setPictures={setPictures}
-                    setFiles={setFiles}
-                    setDuplicateImageModal={setDuplicateImageModal}
-                    identifierOfPicture={identifierOfPicture}
-                    uuid={uuid}
-                    index={index}
-                    dispatch={dispatch}
-                    duplicateImageModal={duplicateImageModal}
-                    removeImage={removeImage}
-                    setRemoveImageModal={setRemoveImageModal}
-                    thumbPhoto={thumbPhoto}
-                    setThumbPhoto={setThumbPhoto}
-                    removeImageModal={removeImageModal}
-                />
+      <div style={fileContainerUploadPicturesWrapperClass} className="uploadPicturesWrapper">
+        <div key={index} style={fileContainerUploadPictureContainerClass} className="uploadPictureContainer">
+          <CardControls
+            duplicateImage={duplicateImage}
+            filesList={filesList}
+            pictures={pictures}
+            picture={picture}
+            setPictures={setPictures}
+            setFiles={setFiles}
+            setDuplicateImageModal={setDuplicateImageModal}
+            identifierOfPicture={identifierOfPicture}
+            uuid={uuid}
+            index={index}
+            dispatch={dispatch}
+            duplicateImageModal={duplicateImageModal}
+            removeImage={removeImage}
+            setRemoveImageModal={setRemoveImageModal}
+            thumbPhoto={thumbPhoto}
+            setThumbPhoto={setThumbPhoto}
+            removeImageModal={removeImageModal}
+          />
 
-                {cardInfo ? (
-                    <RImg
-                        src={product.url}
-                        style={image}
-                        container={children =>
-                            !initialStatus || initialStatus === 'waitingInfo' || editing ? (
-                                <CardInputs image={children || null} update={update || null} index={index}
-                                            arrayOfInputs={arrayOfInputs} validations={validations}
-                                            secondArrayOfInputs={secondArrayOfInputs}/>
-                            ) : initialStatus === 'unavailable' && cartProduct.status !== 'closed' ? (
-                                <InfoCard product={{ requestedQuantities: {}, ...state, ...cartProduct }}
-                                          image={children} setEditing={setEditing}/>
-                            ) : (
-                                <SummaryCard product={{ requestedQuantities: {}, ...state, ...cartProduct }}
-                                             image={children} setEditing={setEditing}/>
-                            )
-                        }
-                        loaderContainer={() => <SpinnerWithDiv/>}
-                    />
+          {cardInfo ? (
+            <RImg
+              src={product.url}
+              style={image}
+              container={children =>
+                !initialStatus || initialStatus === 'waitingInfo' || editing ? (
+                  <CardInputs
+                    image={children || null}
+                    update={update || null}
+                    index={index}
+                    arrayOfInputs={arrayOfInputs}
+                    validations={validations}
+                    secondArrayOfInputs={secondArrayOfInputs}
+                  />
+                ) : initialStatus === 'unavailable' && cartProduct.status !== 'closed' ? (
+                  <InfoCard product={{ requestedQuantities: {}, ...state, ...cartProduct }} image={children} setEditing={setEditing} />
                 ) : (
-                    <RImg
-                        src={picture}
-                        style={fileContainerUploadPictureContainerimgUploadPictureClass}
-                        alt="preview"
-                        container={children => (
-                            <CardInputs disabled image={children || null} update={update || null}
-                                        arrayOfInputs={arrayOfInputs} validations={[]} index={index}/>
-                        )}
-                    />
-                )}
-            </div>
+                  <SummaryCard product={{ requestedQuantities: {}, ...state, ...cartProduct }} image={children} setEditing={setEditing} />
+                )
+              }
+              loaderContainer={() => <SpinnerWithDiv />}
+            />
+          ) : (
+            <RImg
+              src={picture}
+              style={fileContainerUploadPictureContainerimgUploadPictureClass}
+              alt="preview"
+              container={children => (
+                <CardInputs disabled image={children || null} update={update || null} arrayOfInputs={arrayOfInputs} validations={[]} index={index} />
+              )}
+            />
+          )}
         </div>
-    );
-}
+      </div>
+    )
+  },
+)
--- src/App/UploadImages/index.js
+++ src/App/UploadImages/index.js
@@ -1,149 +1,145 @@
-import React, { useContext, useEffect, useReducer, useState } from 'react';
-import Button from '@bit/vitorbarbosa19.ziro.button';
-import ImageUpload from '@bit/vitorbarbosa19.ziro.image-upload';
-import Spinner from '@bit/vitorbarbosa19.ziro.spinner-with-div';
-import { v4 as uuid } from 'uuid';
-import fetch from './fetch';
-import { cardContainerClass, fileContainerClass } from './styles';
-import sendToBackend from './sendToBackend';
-import {
-    duplicateImage,
-    inputStateControl,
-    isValidBrand,
-    onDragOver,
-    removeImage,
-    settingThePicturesAndFiles,
-} from './functionsUploadImages';
-import Card from '../CardForm';
-import BrandChoose from './BrandChoose';
-import inputs from './inputs';
-import ToastNotification from '../ToastNotification';
-import { userContext } from '../appContext';
+import React, { useContext, useEffect, useReducer, useState } from 'react'
+import Button from '@bit/vitorbarbosa19.ziro.button'
+import ImageUpload from '@bit/vitorbarbosa19.ziro.image-upload'
+import Spinner from '@bit/vitorbarbosa19.ziro.spinner-with-div'
+import { v4 as uuid } from 'uuid'
+import fetch from './fetch'
+import { cardContainerClass, fileContainerClass } from './styles'
+import sendToBackend from './sendToBackend'
+import { duplicateImage, inputStateControl, isValidBrand, onDragOver, removeImage, settingThePicturesAndFiles } from './functionsUploadImages'
+import Card from '../CardForm'
+import BrandChoose from './BrandChoose'
+import inputs from './inputs'
+import ToastNotification from '../ToastNotification'
+import { userContext } from '../appContext'
 
 const UploadImages = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    const [isError, setIsError] = useState(false);
-    const [isSubmitting, setIsSubmitting] = useState(false);
-    const [brands, setBrands] = useState([]);
-    const [brandsAndTrends, setBrandsAndTrends] = useState('');
-    const [brand, setBrand] = useState('');
-    const [pictures, setPictures] = useState([]);
-    const [filesList, setFiles] = useState([]);
-    const [isSubmitted, setIsSubmitted] = useState(false);
-    const [showUpload, setShowUpload] = useState(false);
-    const [showButtonTop, setShowButtonTop] = useState(false);
-    const [showButtonBot, setShowButtonBot] = useState(false);
-    const [openToast, setOpenToast] = useState(false);
-    const [typeOfToast, setTypeOfToast] = useState('alert');
-    const [messageToast, setMessageToast] = useState('');
-    const [thumbPhoto, setThumbPhoto] = useState('');
-    const [states, dispatch] = useReducer((state, payload) => inputStateControl(state, payload), {});
-    const defaultQuantityValue = 2;
-    const { device } = useContext(userContext);
-    useEffect(() => fetch(setIsLoading, setIsError, setBrands, setBrandsAndTrends), []);
-    useEffect(() => {
-        if (filesList.length === 0) setThumbPhoto('');
-    }, [filesList]);
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [brands, setBrands] = useState([])
+  const [brandsAndTrends, setBrandsAndTrends] = useState('')
+  const [brand, setBrand] = useState('')
+  const [pictures, setPictures] = useState([])
+  const [filesList, setFiles] = useState([])
+  const [isSubmitted, setIsSubmitted] = useState(false)
+  const [showUpload, setShowUpload] = useState(false)
+  const [showButtonTop, setShowButtonTop] = useState(false)
+  const [showButtonBot, setShowButtonBot] = useState(false)
+  const [openToast, setOpenToast] = useState(false)
+  const [typeOfToast, setTypeOfToast] = useState('alert')
+  const [messageToast, setMessageToast] = useState('')
+  const [thumbPhoto, setThumbPhoto] = useState('')
+  const [oldPictures, setOldPictures] = useState([''])
+  const [states, dispatch] = useReducer((state, payload) => inputStateControl(state, payload), {})
+  const defaultQuantityValue = 2
+  const { device } = useContext(userContext)
+  useEffect(() => fetch(setIsLoading, setIsError, setBrands, setBrandsAndTrends), [])
+  useEffect(() => {
+    if (filesList.length === 0) setThumbPhoto('')
+  }, [filesList])
 
-    useEffect(() => {
-        if (pictures[0]) setShowButtonBot(true);
-        else setShowButtonBot(false);
-        if (pictures[1]) setShowButtonTop(true);
-        else setShowButtonTop(false);
-    }, [pictures]);
+  useEffect(() => {
+    if (pictures[0]) setShowButtonBot(true)
+    else setShowButtonBot(false)
+    if (pictures[1]) setShowButtonTop(true)
+    else setShowButtonTop(false)
+    if (oldPictures !== pictures) setOldPictures(pictures)
+  }, [pictures])
 
-    useEffect(() => {
-        if (isValidBrand(brands, brand)) {
-            setShowUpload(true);
-        } else setShowUpload(false);
-    }, [brand]);
+  useEffect(() => {
+    if (isValidBrand(brands, brand)) {
+      setShowUpload(true)
+    } else setShowUpload(false)
+  }, [brand])
 
-    if (isLoading) return <Spinner size="5rem"/>;
-    // if (isError) return <Error />;
-    const state = {
-        setIsSubmitting,
-        setIsSubmitted,
-        setBrand,
-        states,
-        brand,
-        brandsAndTrends,
-        filesList,
-        setPictures,
-        setFiles,
-        dispatch,
-        thumbPhoto,
-        setThumbPhoto,
-        setOpenToast,
-        setMessageToast,
-        setTypeOfToast,
-    };
-    return (
-        <>
-            <BrandChoose isSubmitting={isSubmitting} brand={brand} setBrand={setBrand} brands={brands}/>
-            <div style={fileContainerClass} className="fileContainer" onDragOver={onDragOver}>
-                <ToastNotification openToastRoot={openToast} setOpenToastRoot={setOpenToast}
-                                   messageToastRoot={messageToast} type={typeOfToast}/>
-                {showUpload && (
-                    <>
-                        <ImageUpload
-                            sendToBackend={data =>
-                                settingThePicturesAndFiles(
-                                    data,
-                                    setIsError,
-                                    pictures,
-                                    filesList,
-                                    setPictures,
-                                    setFiles,
-                                    uuid,
-                                    states,
-                                    dispatch,
-                                    thumbPhoto,
-                                    setThumbPhoto,
-                                )
-                            }
-                            isDisabled={!isValidBrand(brands, brand) || isSubmitting}
-                        />
-                        <div style={cardContainerClass}>
-                            {showButtonTop && (
-                                <>
-                                    <Button click={() => sendToBackend(state)} submitting={isSubmitting}
-                                            cta="Enviar todas fotos" type="button"/>
-                                </>
-                            )}
+  if (isLoading) return <Spinner size="5rem" />
+  // if (isError) return <Error />;
+  const state = {
+    setIsSubmitting,
+    setIsSubmitted,
+    setBrand,
+    states,
+    brand,
+    brandsAndTrends,
+    filesList,
+    setPictures,
+    setFiles,
+    dispatch,
+    thumbPhoto,
+    setThumbPhoto,
+    setOpenToast,
+    setMessageToast,
+    setTypeOfToast,
+  }
+  let cont = 0
+  console.log('render inside UploadImages', cont++)
+  return (
+    <>
+      <BrandChoose isSubmitting={isSubmitting} brand={brand} setBrand={setBrand} brands={brands} />
+      <div style={fileContainerClass} className="fileContainer" onDragOver={onDragOver}>
+        <ToastNotification openToastRoot={openToast} setOpenToastRoot={setOpenToast} messageToastRoot={messageToast} type={typeOfToast} />
+        {showUpload && (
+          <>
+            <ImageUpload
+              sendToBackend={data =>
+                settingThePicturesAndFiles(
+                  data,
+                  setIsError,
+                  pictures,
+                  filesList,
+                  setPictures,
+                  setFiles,
+                  uuid,
+                  states,
+                  dispatch,
+                  thumbPhoto,
+                  setThumbPhoto,
+                )
+              }
+              isDisabled={!isValidBrand(brands, brand) || isSubmitting}
+            />
+            <div style={cardContainerClass}>
+              {showButtonTop && (
+                <>
+                  <Button click={() => sendToBackend(state)} submitting={isSubmitting} cta="Enviar todas fotos" type="button" />
+                </>
+              )}
 
-                            {pictures.map((picture, index) => {
-                                return (
-                                    <div key={index}>
-                                        <Card
-                                            key={index}
-                                            identifierOfPicture={picture.identifier}
-                                            states={states}
-                                            filesList={filesList}
-                                            setFiles={setFiles}
-                                            index={index}
-                                            picture={picture.urlImage}
-                                            removeImage={removeImage}
-                                            duplicateImage={duplicateImage}
-                                            arrayOfInputs={inputs(states, picture.identifier, dispatch, defaultQuantityValue, device, isSubmitting)}
-                                            pictures={pictures}
-                                            setPictures={setPictures}
-                                            dispatch={dispatch}
-                                            uuid={uuid}
-                                            thumbPhoto={thumbPhoto}
-                                            setThumbPhoto={setThumbPhoto}
-                                        />
-                                    </div>
-                                );
-                            })}
-                            {showButtonBot && (
-                                <Button click={() => sendToBackend(state)} submitting={!showButtonBot || isSubmitting}
-                                        cta="Enviar todas fotos" type="button"/>
-                            )}
-                        </div>
-                    </>
-                )}
+              {pictures === oldPictures &&
+                pictures.map((picture, index) => {
+                  console.log('index inside the MAP UploadImages', index)
+                  return (
+                    <div key={index}>
+                      <Card
+                        key={index}
+                        identifierOfPicture={picture.identifier}
+                        states={states}
+                        filesList={filesList}
+                        setFiles={setFiles}
+                        index={index}
+                        picture={picture.urlImage}
+                        removeImage={removeImage}
+                        duplicateImage={duplicateImage}
+                        arrayOfInputs={inputs(states, picture.identifier, dispatch, defaultQuantityValue, device, isSubmitting)}
+                        pictures={pictures}
+                        setPictures={setPictures}
+                        dispatch={dispatch}
+                        uuid={uuid}
+                        thumbPhoto={thumbPhoto}
+                        setThumbPhoto={setThumbPhoto}
+                      />
+                    </div>
+                  )
+                })}
+              {showButtonBot && (
+                <Button click={() => sendToBackend(state)} submitting={!showButtonBot || isSubmitting} cta="Enviar todas fotos" type="button" />
+              )}
             </div>
-        </>
-    );
+          </>
+        )}
+      </div>
+    </>
+  )
 }
-export default UploadImages;
+export default UploadImages
